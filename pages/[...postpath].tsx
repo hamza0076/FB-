d27117f58aa2c59644d2339595b485b7b342e559
hamza0076 <112745373+hamza0076@users.noreply.github.com>
2023-08-22 17:32:36 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://webbift.com` + encodeURI(path as string)
+					`https://webbift.com`
 				}`,
 			},
 		};
